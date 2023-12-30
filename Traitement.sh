26cf44e53aa2fd6eb5dba9d359fdeb25d0861ec9
Sikaaaa <117377996+Sikaaaa@users.noreply.github.com>
2023-12-30 20:49:12 +0100

    Add files via upload

--- Traitement.sh
+++ Traitement.sh
@@ -4,93 +4,94 @@
 nombre_arg=$*
 
 
-
-if [ -z $nombre_arg ] ; then 
-    echo "-d1 -> Conducteurs avec le plus de trajets"
-    echo "-d2 -> Conducteurs avec la plus grande distance"
-    echo "-l -> Les 10 trajets les plus longs"
-    echo "-t -> Les 10 villes les plus traversées"
-    echo "-s -> Statistiques sur les étapes"
-
-else 
-    case $nombre_arg in
-    '-d1') #récupérer les 10 conducteurs avec le plus de trajets 
-    # Récupere l'heure au début de l'exe
-    tmp_d=$(date +%s)
+case $nombre_arg in
+'-d1') #récupérer les 10 conducteurs avec le plus de trajets 
+# Récupere l'heure au début de l'exe
+tmp_d=$(date +%s)
     
     #récupérer champs 2 (tout 1) et 6 (trier nom prénom ordre décroissant en prenant le nom) 
-    awk -F';' '$2 == 1 {tab[$6] += 1 }
-        END {for (i in tab) printf "%d, %s \n", tab[i],i}' ./data/data.csv > ./temp/d1_tableau.csv
+awk -F';' '$2 == 1 {tab[$6] += 1 }
+    END {for (i in tab) printf "%d, %s \n", tab[i],i}' ./data/data.csv > ./temp/d1_tableau.csv
 
-    sort -n -r -t';' -k1 ./temp/d1_tableau.csv > ./temp/d1_tri.csv
-    head -10 ./temp/d1_tri.csv > ./data/d1_top_10.csv
+sort -n -r -t';' -k1 ./temp/d1_tableau.csv > ./temp/d1_tri.csv
+head -10 ./temp/d1_tri.csv > ./data/d1_top_10.csv
 
-    # Récupere l'heure à la fin de l'exe
-    tmp_f=$(date +%s)
+# Récupere l'heure à la fin de l'exe
+tmp_f=$(date +%s)
 
-    # Calcule le temps d'exe en soustraillant les deux
-    echo "Le temps d'execution est de" $((tmp_f - tmp_d)) "secondes"
+# Calcule le temps d'exe en soustraillant les deux
+echo "Le temps d'execution est de" $((tmp_f - tmp_d)) "secondes"
 
-    #générer le graphique
-    gnuplot d1.gnu
+#générer le graphique
+gnuplot d1.gnu
 
-    ;;
+;;
 
-    '-d2') #Récupérer les 10 conducteurs avec le plus de km au compteur
+'-d2') #Récupérer les 10 conducteurs avec le plus de km au compteur
 
-    # Récupere l'heure au début de l'exe
-    tmp_d=$(date +%s)
+# Récupere l'heure au début de l'exe
+tmp_d=$(date +%s)
 
-    awk -F';' '{tab[$6] += $5}
-        END {for (i in tab) printf "%d, %s \n", tab[i],i}' ./data/data.csv > ./temp/d2_tableau.csv
-    sort -n -r -t';' -k1 ./temp/d2_tableau.csv > ./temp/d2_tri.csv
-    head -10 ./temp/d2_tri.csv > ./data/d2_top_10.csv
+awk -F';' '{tab[$6] += $5}
+    END {for (i in tab) printf "%d, %s \n", tab[i],i}' ./data/data.csv > ./temp/d2_tableau.csv
+sort -n -r -t';' -k1 ./temp/d2_tableau.csv > ./temp/d2_tri.csv
+head -10 ./temp/d2_tri.csv > ./data/d2_top_10.csv
 
-    # Récupere l'heure à la fin de l'exe
-    tmp_f=$(date +%s)
+# Récupere l'heure à la fin de l'exe
+tmp_f=$(date +%s)
 
-    # Calcule le temps d'exe en soustraillant les deux
-    echo "Le temps d'execution est de" $((tmp_f - tmp_d)) "secondes"
+# Calcule le temps d'exe en soustraillant les deux
+echo "Le temps d'execution est de" $((tmp_f - tmp_d)) "secondes"
 
-    #générer le graphique 
-    gnuplot d2.gnu
-    ;;
+#générer le graphique 
+gnuplot d2.gnu
+;;
 
-    '-l') # les 10 trajets les plus longs
+'-l') # les 10 trajets les plus longs
 
-    # Récupere l'heure au début de l'exe
-    tmp_d=$(date +%s)
+# Récupere l'heure au début de l'exe
+tmp_d=$(date +%s)
     
-    #récupérer somme chaque étape 
-    awk -F';' '{tab[$1] += $5} 
-        END {for (i in tab) printf "%d, %d \n", i,tab[i]}' ./data/data.csv > ./temp/l_tableau.csv
+#récupérer somme chaque étape 
+awk -F';' '{tab[$1] += $5} 
+    END {for (i in tab) printf "%d, %d \n", i,tab[i]}' ./data/data.csv > ./temp/l_tableau.csv
         
-    sort -n -r -t',' -k2 ./temp/l_tableau.csv > ./temp/l_tri.csv
-    head -10 ./temp/l_tri.csv > ./temp/l_tri2.csv
-    sort -n -t',' -k1 ./temp/l_tri2.csv > ./data/l_trajet_plus_long.csv
+sort -n -r -t',' -k2 ./temp/l_tableau.csv > ./temp/l_tri.csv
+head -10 ./temp/l_tri.csv > ./temp/l_tri2.csv
+sort -n -t',' -k1 ./temp/l_tri2.csv > ./data/l_trajet_plus_long.csv
 
-    # Récupere l'heure à la fin de l'exe
-    tmp_f=$(date +%s)
+# Récupere l'heure à la fin de l'exe
+tmp_f=$(date +%s)
 
-    # Calcule le temps d'exe en soustraillant les deux
-    echo "Le temps d'execution est de" $((tmp_f - tmp_d)) "secondes"
+# Calcule le temps d'exe en soustraillant les deux
+echo "Le temps d'execution est de" $((tmp_f - tmp_d)) "secondes"
 
-    #générer le graphique
-    gnuplot l.gnu
-    ;; 
+#générer le graphique
+gnuplot l.gnu
+;; 
 
-    '-t')
-    #faire l'option t
-    ;;
-    '-s')
-    #faire l'option s
-    ;;
-    *)
-    echo "bla bla"
-    ;;
-    esac
+'-t')
+#faire l'option t
+;;
+'-s')
+#faire l'option s
+;;
+
+'-h')
+echo "Pour choisir les options de graphiques il est possible de faire :"
+echo " -d1 pour avoir les 10 conducteurs avec le plus de trajets à leur actif"
+echo " -d2 pour avoir les 10 conducteurs ayant parcouru le plus de kilomètres"
+echo " -l pour avoir les 10 trajets les plus longs"
+echo " -t pour avoir les 10 villes les plus traversées"
+echo " -s pour avoir les différentes statistiques sur les étapes"
+
+;;
+
+*)
+echo "Pour avoir les options disponibles, essayez -h"
+;;
+esac
     
-    fi
 
 
 
--- l.gnu
+++ l.gnu
@@ -1,20 +1,18 @@
 #Paramètres du graphique
 set terminal png
 set title "Les 10 trajets les plus longs"
-set xlabel "Id Route"
-set ylabel "Distance en km"
+set xlabel "Distance en km"
+set ylabel "Id Route"
 
 #nom du fichier dans lequel le graphique apparaitra 
 set output "./image/Les 10 trajets les plus longs.png" 
 
 # Type de graphique
-set style data histograms
 set style fill solid
-set boxwidth 0.4
-
-# Pour que le graph soit dans le bon sens 
-set yrange [0.5:*]
-set xrange [0.5:*]
+set boxwidth 0.7
+set size 1,1
+set key font "Times New Roman, 13"
+set border lc rgb 'purple'
 
 # Charger les données depuis le fichier
-plot './data/l_trajet_plus_long.csv' using 1:xticlabels(2) title "T" with boxes
+plot './data/l_trajet_plus_long.csv' using 1:xticlabels(2) title "Distance" with boxes
